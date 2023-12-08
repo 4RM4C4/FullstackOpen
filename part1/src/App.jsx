1b0f62deeb47b60ebcf169e85a92ffd600d6383e
Ariel Castro <arielmatiascastro@gmail.com>
2023-12-08 19:44:40 -0300

    Commit Part1 exercise 1.8
    Part1 exercise 1.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 const Title = ({text}) => <h1>{text}</h1>
+
+const Statistics = ({good, neutral, bad}) => (
+  <>
+      <Title text='Statistics'/><br/>
+      <Show item={good} text='Good: '/>
+      <Show item={neutral} text='Neutral: '/>
+      <Show item={bad} text='Bad: '/>
+      <Sum item1={good} item2={neutral} item3={bad} text='All:'/>
+      <Avg item1={good} item2={neutral} item3={bad} text='Average:'/>
+      <Pos item1={good} item2={neutral} item3={bad} text='Positive:'/>
+  </>
+      )
+
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
 const Show =  ({item, text}) => <div>{text} {item}</div>
+
 const Sum =  ({item1, item2, item3,  text}) => <div>{text} {item1+item2+item3}</div>
+
 const Avg =  ({item1, item2, item3,  text}) => {
   if((item1+item2+item3)==0) {
-    return <div>{text} There is not enough data to calculate an average.</div>
+    return <div>{text} There is not enough data to calculate.</div>
   }
   return <div>{text} {(item1+(item3*-1))/(item1+item2+item3)}</div>
 }
+
 const Pos = ({item1, item2, item3,  text}) => {
   if((item1+item2+item3)==0) {
     return <div>{text} There is not enough data to calculate.</div>
@@ -29,14 +46,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral+1)} text='Neutral'/>
       <Button handleClick={() => setBad(bad+1)} text='Bad'/>
       <br/>
-      <Title text='Statistics'/>
-      <br/>
-      <Show item={good} text='Good: '/>
-      <Show item={neutral} text='Neutral: '/>
-      <Show item={bad} text='Bad: '/>
-      <Sum item1={good} item2={neutral} item3={bad} text='All:'/>
-      <Avg item1={good} item2={neutral} item3={bad} text='Average:'/>
-      <Pos item1={good} item2={neutral} item3={bad} text='Positive:'/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
