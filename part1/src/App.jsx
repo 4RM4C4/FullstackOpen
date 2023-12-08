345e285ea80b0d5b972056b65b89f2c969febf60
Ariel Castro <arielmatiascastro@gmail.com>
2023-12-08 19:26:12 -0300

    Commit Part1 exercise 1.7
    Part1 exercise 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,8 +1,20 @@
 import { useState } from 'react'
-
+const Title = ({text}) => <h1>{text}</h1>
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 const Show =  ({item, text}) => <div>{text} {item}</div>
-const Title = ({text}) => <h1>{text}</h1>
+const Sum =  ({item1, item2, item3,  text}) => <div>{text} {item1+item2+item3}</div>
+const Avg =  ({item1, item2, item3,  text}) => {
+  if((item1+item2+item3)==0) {
+    return <div>{text} There is not enough data to calculate an average.</div>
+  }
+  return <div>{text} {(item1+(item3*-1))/(item1+item2+item3)}</div>
+}
+const Pos = ({item1, item2, item3,  text}) => {
+  if((item1+item2+item3)==0) {
+    return <div>{text} There is not enough data to calculate.</div>
+  }
+return <div>{text} {(item1*100)/(item1+item2+item3)}%</div>
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -22,6 +34,9 @@ const App = () => {
       <Show item={good} text='Good: '/>
       <Show item={neutral} text='Neutral: '/>
       <Show item={bad} text='Bad: '/>
+      <Sum item1={good} item2={neutral} item3={bad} text='All:'/>
+      <Avg item1={good} item2={neutral} item3={bad} text='Average:'/>
+      <Pos item1={good} item2={neutral} item3={bad} text='Positive:'/>
     </div>
   )
 }
