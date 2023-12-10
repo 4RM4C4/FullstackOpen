cc85eb67bb53232c0929234c49efc6c5fa031c3a
Ariel Castro <arielmatiascastro@gmail.com>
2023-12-10 18:27:29 -0300

    Commit Part2 exercise 2.4 & 2.5 Courseinfo
    Part2 exercise 2.4 & 2.5 Courseinfo

--- part2/courseinfo/src/components/Header.jsx
+++ part2/courseinfo/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Header = ({header}) => <h1>{header}</h1>
+const Header = ({header}) => <h2>{header}</h2>
 
 export default Header
--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Total = ({total}) => <p>Number of exercises: {total.reduce((sum, order) => sum+order.exercises, 0)}</p>
+const Total = ({total}) => <p><strong>Number of exercises: {total.reduce((sum, order) => sum+order.exercises, 0)}</strong></p>
 
 export default Total
