2325277a875e59f6d91659e0610457aafdc563ca
Ariel Castro <arielmatiascastro@gmail.com>
2023-12-11 11:30:33 -0300

    Commit Part2 exercise 2.9 Phonebook
    Part2 exercise 2.9 Phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567',
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -15,6 +17,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
   }
 
   const addName = (event) => {
@@ -30,18 +36,21 @@ const App = () => {
     }
     setNewName('')
     setNewNumber('')
+    setFilter('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div> Filter shown with: <input value={filter} onChange={handleFilterChange} /> </div>
+      <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div> name: <input value={newName} onChange={handleNameChange} /> </div>
         <div> number: <input value={newNumber} onChange={handleNumberChange} /> </div>
         <div> <button type="submit">add</button> </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, position) => <p key={position}>{person.name} {person.number}</p>)}
+      {filter === '' ? persons.map((person, position) => <p key={position}>{person.name} {person.number}</p>) : persons.map((person, position) => (person.name.toLowerCase().includes(filter.toLowerCase()) || person.number.includes(filter) ? <p key={position}>{person.name} {person.number}</p> : ''))}
     </div>
   )
 }
