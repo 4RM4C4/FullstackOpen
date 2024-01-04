4fac0d8e7ff7031d7c3307c010f74fc6998623ad
Ariel Castro <arielmatiascastro@gmail.com>
2024-01-04 14:19:04 -0300

    Commit Part3 exercise 3.16 phonebook database step 4
    Part3 exercise 3.16 phonebook database step 4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,8 +7,24 @@ const Person = require('./models/person')
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+
 
 
 let persons = [
@@ -34,38 +50,39 @@ let persons = [
     }
 ]
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({})
         .then(persons => {
             response.json(persons)
         })
+        .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     Person.find({})
-    .then(persons => {
-        response.send(`<p>Phonebook has info for ${persons.length} people</p><br/><p>${Date()}</p>`)
-    })   
+        .then(persons => {
+            response.send(`<p>Phonebook has info for ${persons.length} people</p><br/><p>${Date()}</p>`)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
             response.json(person)
         })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
         })
-        .catch(error => {
-            response.status(400).send({ error: 'malformatted id' })
-        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -85,9 +102,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 
