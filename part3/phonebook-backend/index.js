db9c516edd7921d18fe4fd34230ea07c020d8b7e
Ariel Castro <arielmatiascastro@gmail.com>
2024-01-04 12:37:18 -0300

    Commit Part3 exercise 3.15 phonebook database step 3
    Part3 exercise 3.15 phonebook database step 3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -10,6 +10,7 @@ app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+
 let persons = [
     {
         "id": 1,
@@ -34,25 +35,34 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        })
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><br/><p>${Date()}</p>`)
+    Person.find({})
+    .then(persons => {
+        response.send(`<p>Phonebook has info for ${persons.length} people</p><br/><p>${Date()}</p>`)
+    })   
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-      })
+    Person.findById(request.params.id)
+        .then(person => {
+            response.json(person)
+        })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            response.status(400).send({ error: 'malformatted id' })
+        })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -66,7 +76,7 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({ error: 'number missing' })
     }
 
-    if(persons.find(element => element.name === body.name)){
+    if (persons.find(element => element.name === body.name)) {
         return response.status(400).json({ error: 'name must be unique' })
     }
 
@@ -77,7 +87,7 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(savedPerson => {
         response.json(savedPerson)
-      })
+    })
 })
 
 
