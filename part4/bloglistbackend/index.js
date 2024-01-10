fb28cd4168da22b4e858c1fbb3b134dfdc8571e3
Ariel Castro <arielmatiascastro@gmail.com>
2024-01-10 17:42:06 -0300

    Commint Part4 exercise 4.2 BlogListBackend step2
    Part4 exercise 4.2 BlogListBackend step2

--- part4/bloglistbackend/index.js
+++ part4/bloglistbackend/index.js
@@ -1,60 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-},
-  {
-    toJSON: {
-      transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-      },
-    },
-  })
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const MONGODB_URI = process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.get('/api/blogs/:id', (request, response) => {
-  Blog
-  .findById(request.params.id)
-  .then((blog) => {
-    response.json(blog)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
