c12d19bbec9c879eb8057db2308a1471f9ffdfce
Ariel Castro <arielmatiascastro@gmail.com>
2024-01-10 19:50:19 -0300

    Commit Part4 exercise 4.4 helper functions and unit tests step2
    Part4 exercise 4.4 helper functions and unit tests step2

--- part4/bloglistbackend/utils/list_helper.js
+++ part4/bloglistbackend/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+return blogs.lenght === 0 ? 0 : blogs.reduce((sum, current) => sum+current.likes,0)
+} 
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
