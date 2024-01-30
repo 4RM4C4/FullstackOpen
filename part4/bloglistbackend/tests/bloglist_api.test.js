6ddd37e86ca698bfca488b5b161de6ff2e88f21f
Ariel Castro <arielmatiascastro@gmail.com>
2024-01-30 20:59:25 -0300

    Commit Part4 exercise 4.11 bloglist tests step4
    Part4 exercise 4.11 bloglist tests step4

--- part4/bloglistbackend/tests/bloglist_api.test.js
+++ part4/bloglistbackend/tests/bloglist_api.test.js
@@ -6,27 +6,27 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-  "title": "Tengo un titulo muy malo",
-  "author": "Bigote Lactico",
-  "url": "midireccion.com",
-  "likes": 69,
-  "id": "659eed6260b672aa2cd2ea54"
+    "title": "Tengo un titulo muy malo",
+    "author": "Bigote Lactico",
+    "url": "midireccion.com",
+    "likes": 69,
+    "id": "659eed6260b672aa2cd2ea54"
   },
   {
-  "title": "Otro Titulo Malardo",
-  "author": "Bijeator",
-  "url": "midireccionbijeada.com",
-  "likes": 69,
-  "id": "659ef0c0792a7626259440ef"
+    "title": "Otro Titulo Malardo",
+    "author": "Bijeator",
+    "url": "midireccionbijeada.com",
+    "likes": 69,
+    "id": "659ef0c0792a7626259440ef"
   },
   {
-  "title": "El Blanco lado de la luna",
-  "author": "Blanco Shinso",
-  "url": "Shinso.blanco.com",
-  "likes": 69,
-  "id": "659f0074291dae16f691c71d"
+    "title": "El Blanco lado de la luna",
+    "author": "Blanco Shinso",
+    "url": "Shinso.blanco.com",
+    "likes": 69,
+    "id": "659f0074291dae16f691c71d"
   }
-  ]
+]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -56,8 +56,8 @@ test('post successfully creates a new blog', async () => {
     "author": "Armaca",
     "url": "armaca.com.ar",
     "likes": 121,
-    }
-    await api
+  }
+  await api
     .post('/api/blogs')
     .send(newBlog)
     .expect(201)
@@ -71,6 +71,25 @@ test('post successfully creates a new blog', async () => {
   expect(titles).toContain(
     'El oscuro lado de la programación'
   )
+})
+
+test('post with likes missing set likes value to 0', async () => {
+  const newBlog = {
+    "title": "El oscuro lado de la programación",
+    "author": "Armaca",
+    "url": "armaca.com.ar",
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const likes = response.body.map(r => r.likes)
+
+  expect(likes[initialBlogs.length]).toEqual(0)
 })
 
 afterAll(async () => {
